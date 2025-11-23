366dc76dcb9faa34f6c9e11d978e0f963cac7ad2
m4rkbello <markbello10041999@gmail.com>
2025-11-23 15:57:02 +0800

    ADD     "start": "node server.js" AS NP RUN START FOR  DEPLOYMENT PURPOSES ONLY - FIXED

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,26 @@ const express = require("express");
 
 const app = express();
 
+//fetch data
 app.get("/api/notes", (req, res) =>{
-    res.send("you got 5 hehe patotoya!");
+    res.status(200).send("Note fetch successfully!");
 });
+
+//create data
+app.post("api/notes", (req, res) => {
+    res.status(201).json({message: "Note created successfully!"})
+});
+
+//update data
+app.put("api/notes/:id", (req, res) => {
+    res.status(200).json({message: "Note updated successfully!"})
+});
+
+
+app.delete("api/notes/:id", (req, res) => {
+    res.status(200).json({message: "Note deleted successfully!"})
+});
+
 
 app.listen(5001, () => {
     console.log("SERVER STARTED ON PORT 5001!");
