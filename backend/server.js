d21807911ec5ddb011f38694f3b2e944d1b04b14
m4rkbello <markbello10041999@gmail.com>
2025-11-23 15:15:59 +0800

    NODEJS BACKEND

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/api/notes", (req, res) =>{
+    res.send("you got 5 notes patotoya!");
+});
+
 app.listen(5001, () => {
     console.log("SERVER STARTED ON PORT 5001!");
 });
