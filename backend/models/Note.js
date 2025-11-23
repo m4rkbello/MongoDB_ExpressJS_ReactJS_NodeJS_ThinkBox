81a3735929dd2e9f019c74015d5308d550650097
m4rkbello <markbello10041999@gmail.com>
2025-11-23 21:10:59 +0800

    ADD SCHEMA AND MODEL USING EXPRESS JS AND NODEJS

---
+++ backend/models/Note.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+//1- Create schema
+//2- create model based off  that schema
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+},{timestamps: true});
+
+const Note = mongoose.model("Note", noteSchema)
+
+export default Note;
