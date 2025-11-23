6f8b160cff7d72e6ec52440ccd0e639970774edf
m4rkbello <markbello10041999@gmail.com>
2025-11-23 19:25:41 +0800

    INSTALL MONGOOSE CONNECTION FOR MONGODB

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,7 @@ app.use("/api/notes", notesRoutes);
 app.listen(5001, () => {
     console.log("SERVER STARTED ON PORT 5001!");
 });
+
+
+
+// mongodb+srv://markamarcortejopanesbello_db_user:0YN3xbFCtIlpvCQJ@cluster0.owk8mag.mongodb.net/?appName=Cluster0
