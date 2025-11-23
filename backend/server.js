a0f88a24e58106a71705886d89840f30d3e9f1d5
m4rkbello <markbello10041999@gmail.com>
2025-11-23 20:04:37 +0800

    ADD DATABASE CONNECTION FROM MONGOOSE TO MONGODB AND ADD ENV AND CONFIGURE IT TO NOT TO EXPOSE IT

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
@@ -11,4 +17,4 @@ app.listen(5001, () => {
 
 
 
-// mongodb+srv://markamarcortejopanesbello_db_user:0YN3xbFCtIlpvCQJ@cluster0.owk8mag.mongodb.net/?appName=Cluster0
+// 
