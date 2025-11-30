02be794e10e182c8cb04b7b79249177893dbbb40
m4rkbello <markbello10041999@gmail.com>
2025-11-30 15:30:29 +0800

    ADDED

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -2,7 +2,7 @@
 import Note from "../models/Note.js";
 
 //FETCH NOTES
-export async function getAllNotes(req, res) {
+export async function getAllNotes(_, res) {
     try{
         const notes = await Note.find().sort({ createdAt: -1 });
         res.status(200).json(notes);
