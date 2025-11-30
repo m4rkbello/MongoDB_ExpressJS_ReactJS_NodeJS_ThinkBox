ab62d3cbc149d163f65178439743a56c4ca36c3d
m4rkbello <markbello10041999@gmail.com>
2025-11-30 20:58:20 +0800

    IMPLEMENTING RATE LIMITER ON THE SERVER

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -12,6 +13,9 @@ connectDB();
 
 //Middleware
 app.use(express.json());
+
+app.use(rateLimiter)
+
 
 app.use("/api/notes", notesRoutes);
 
