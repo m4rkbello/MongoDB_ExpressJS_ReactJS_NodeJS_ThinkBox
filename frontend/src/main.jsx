14b264613fbac426b7e89328d9d61d9e52e4e3aa
m4rkbello <markbello10041999@gmail.com>
2025-12-02 21:44:49 +0800

    ADDED

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,10 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router';
-import toast, {Toaster} from 'react-hot-toast';
+import {Toaster} from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <button className='bg-red-300' onClick={() => toast.success('OHAHAY!')}>Click me</button>
     <BrowserRouter>
       <App />
       <Toaster />
