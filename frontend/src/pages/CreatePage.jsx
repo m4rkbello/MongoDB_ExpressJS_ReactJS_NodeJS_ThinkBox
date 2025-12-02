dbe3585e3cec039d18739017283fa5afd9b3fda0
m4rkbello <markbello10041999@gmail.com>
2025-12-02 21:04:27 +0800

    ADDED

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CreatePage = () => {
   return (
     <div>
-      
+      <h1>Create  Note</h1>
     </div>
   )
 }
--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NoteDetailPage = () => {
   return (
     <div>
-      
+      <h1>Note Detail Page</h1>
     </div>
   )
 }
