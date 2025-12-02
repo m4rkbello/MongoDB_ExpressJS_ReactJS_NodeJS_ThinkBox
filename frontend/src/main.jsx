1e0c71821867249fb4786daba38c1d78a6e31bac
m4rkbello <markbello10041999@gmail.com>
2025-12-02 21:23:07 +0800

    INSTALL TAILWIND AND SETUP

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router';
-import {Toaster} from 'react-hot-toast';
+import toast, {Toaster} from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <button className='bg-red-300' onClick={() => toast.success('OHAHAY!')}>Click me</button>
     <BrowserRouter>
       <App />
       <Toaster />
