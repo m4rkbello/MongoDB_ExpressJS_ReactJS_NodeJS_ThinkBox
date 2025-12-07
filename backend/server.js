7584adc2f77bd9851be0e7c3407ddccd5690775b
m4rkbello <markbello10041999@gmail.com>
2025-12-07 20:35:49 +0800

    BACKEND/FRONTEND: ADD MAP METHOD AND SOME DISPLAY RENDERING CONDITION - FIXED

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5001
 
 //Middleware
-app.use(express.json());
-app.use(rateLimiter);
 app.use(cors({  
         origin: 'http://localhost:5173'
     }));
+
+app.use(express.json());
+app.use(rateLimiter);
+
 
 //routes
 app.use("/api/notes", notesRoutes);
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -47,7 +47,22 @@ const HomePage = () => {
     <div className='min-h-screen'>
       <Navbar />
 
-{isRateLimited && <RateLimitedUI />}
+      {isRateLimited && <RateLimitedUI />}
+
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        { loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {/** DISPLAY THE NOTE */}
+            {notes.map((note) => (
+              <div key={note._id} className="border border-gray-300 rounded-lg p-4 shadow hover:shadow-lg transition">
+                <h2 className="text-xl font-semibold mb-2">{note.title}</h2>
+                <p className="text-gray-700">{note.content}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
 
     </div>
   )
