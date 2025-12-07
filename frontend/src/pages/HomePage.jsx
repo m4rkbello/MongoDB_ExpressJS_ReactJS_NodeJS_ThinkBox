dcf62fb8928ed9012e871fc1126f4a0f3fb2a03a
m4rkbello <markbello10041999@gmail.com>
2025-12-07 22:17:08 +0800

    FRONTEND: ADD SKELETON LOADING  USING DAISYUI

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -43,7 +43,6 @@ const HomePage = () => {
 
   },[]);
 
-
   return (
     <div className='min-h-screen'>
       <Navbar />
@@ -51,7 +50,14 @@ const HomePage = () => {
       {isRateLimited && <RateLimitedUI />}
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
-        { loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+        { loading && 
+          <div className="flex w-52 flex-col gap-4">
+            <div className="skeleton h-32 w-full"></div>
+            <div className="skeleton h-4 w-28"></div>
+            <div className="skeleton h-4 w-full"></div>
+            <div className="skeleton h-4 w-full"></div>
+          </div>
+        }
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
             {/** DISPLAY THE NOTE */}
