21bd436307d326865fabd16a400e8f82951fc2fc
m4rkbello <markbello10041999@gmail.com>
2025-12-08 13:12:15 +0800

    FRONTEND: ADD UI MODERN DESIGN -

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -14,7 +14,7 @@ const NoteCard = ({note, setNotes}) => {
 
         try{
             await api.delete(`/notes/${id}`)
-            setNotes((prev) => prev.filter(note => note._id !== id)) //e filter niya ang nadelete ug mawala  sa UI without refreshing the page
+            setNotes((prev) => prev.filter(note => note._id !== id)) //e filter niya ang nadelete ug mawala  sa UI without refreshing
             toast.success("Note deleted successfully!");
         }catch(error){
             console.log("Error deleting note!",error);
@@ -26,6 +26,8 @@ const NoteCard = ({note, setNotes}) => {
   return  (
     <>
         <Link to={`/note/${note._id}`} className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]">
+           <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]">
+            </div></div>
             <div className="card-body">
                 <h3 className="card-title text-base-content">{note.title}</h3>
                 <p className="text-base-content/70 lime-clamp-3">{note.content}</p>
@@ -42,6 +44,8 @@ const NoteCard = ({note, setNotes}) => {
                     </div>
                 </div>
             </div>
+           
+           
          </Link>
     </>
 
