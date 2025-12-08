f51459827490131e03a10cd8a16afe34fdcb0ae8
m4rkbello <markbello10041999@gmail.com>
2025-12-08 13:45:17 +0800

    DEPLOY TO RENDER AS BOTH SAME FOLDER OR SAME DOMAIN

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -9,19 +10,31 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+
+//path sa render
+const ___dirname = path.resolve();
 
 //Middleware
-app.use(cors({  
-        origin: 'http://localhost:5173'
-    }));
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({  
+            origin: 'http://localhost:5173'
+        }));
+};
 
 app.use(express.json());
 app.use(rateLimiter);
 
-
 //routes
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(___dirname,"../frontend/dist")));
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(___dirname,"../frontend","dist","index.html"));
+    });
+};
 
 connectDB().then(() =>{
     app.listen(PORT, () => {
